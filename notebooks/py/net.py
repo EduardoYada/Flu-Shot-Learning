f0161dfc642c8c9cced602a307b24f4618345dbc
Occhima <marcoocchialini@hotmail.com>
2020-10-31 20:08:43 -0300

    finished pycaret implementation

---
+++ notebooks/py/net.py
@@ -0,0 +1,29 @@
+
+import torch
+from torch import nn
+
+
+
+class Net(nn.Net):
+
+    pass
+
+
+
+
+def main():
+    pass
+
+def prep():
+    pass
+
+def train():
+    pass
+
+def metrics():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
